cdc69116e79bc69afc3823b3cc30515e20d46ab0
Shahnawaz <mailtoshahnawazk@gmail.com>
2021-08-02 21:46:15 +0530

    Added_user_login

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
 ]
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Navbar
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
 # Create your views here.
 def index(request):
     navbar=Navbar.objects.all()
     return render(request,'index.html',{'navbars':navbar})
+def login(request):
+    if request.method=='POST':
+        email=request.POST['email']
+        password=request.POST['password']
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.info(request,'Invalid Login')
+            return redirect('login')
+       
+        else:
+            if user.check_password(password):
+                auth.login(request,user)
+                return redirect('/')
+            else:
+                messages.info(request,'Invalid Login')
+                return redirect('login')
+    else:        
+        return render(request,'login.html')
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
